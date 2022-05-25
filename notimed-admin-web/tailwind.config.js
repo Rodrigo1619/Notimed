38fb16c393d56e120b49045791b1081e392f20bb
fernanortega-hub <00191820@uca.edu.sv>
2022-05-24 18:43:13 -0600

    Initialized Next App w/ TailwindCSS

---
+++ notimed-admin-web/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: [
+    "./pages/*/.{js,ts,jsx,tsx}",
+    "./components/*/.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#2a56c8",
+        "onPrimary": "#ffffff",
+        "primaryContainer": "#dae1ff",
+        "onPrimaryContainer": "#00154f",
+        "secondary": "#595e71",
+        "onSecondary": "#ffffff",
+        "secondaryContainer": "#dde1f9",
+        "onSecondaryContainer": "#161b2c",
+        "tertiary": "#745470",
+        "onTertiary": "#ffffff",
+        "tertiaryContainer": "#ffd7f8",
+        "onTertiaryContainer": "#2c122b",
+        "error": "#ba1b1b",
+        "onError": "#ffffff",
+        "errorContainer": "#ffdad4",
+        "onErrorContainer": "#410001",
+        "background": "#fefbff",
+        "onBackground": "#1b1b1f",
+        "surface": "#fefbff",
+        "onSurface": "#1b1b1f",
+        "surface-variant": "#e1e1ec",
+        "onSurface-variant": "#45464f",
+        "outline": "#75767f"
+      }
+    },
+  },
+  plugins: [],
+}
