d2b0ba983c5e59f33b34b5dc0bcd0aebc8c46d1f
juanrodas-654 <00190220@uca.edu.sv>
2022-06-01 19:24:46 -0600

    web promo without responsive

---
+++ notimed-promo-web/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      raleway: ['Raleway', 'sans-serif']  
+    },
+    colors: {
+      'blue-primary': '#2A56C8',
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {}
+  },
+  plugins: [],
+}
