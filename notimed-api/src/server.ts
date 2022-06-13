17ec5c8b9968a1931e252b6f7ea6548d8d0ee250
fernanortega-hub <00191820@uca.edu.sv>
2022-06-13 15:19:44 -0600

    feat: getUser added

--- notimed-api/src/server.ts
+++ notimed-api/src/server.ts
@@ -6,12 +6,11 @@ import passport from 'passport';
 // Constants
 const cors = require('cors');
 const app = express();
-const usersPath = '/notimed';
-const loginPath = '/login';
-const reminderPath = '/reminder';
-const contactPath = '/contact';
-const appointmentPath = '/appointment'
-const profilePath = '/profile';
+const usersPath = '/users';
+const identityPath = '/identity';
+const reminderPath = '/reminders';
+const contactPath = '/contacta';
+const appointmentPath = '/appointments'
 
 //Para tomar los datos del body en formato json
 var bodyParser = require('body-parser');
@@ -33,9 +32,8 @@ app.use(cors());
 //rutas definidas
 
 app.use(usersPath, require('../src/routes/user-routes'));
-app.use(loginPath, require('../src/routes/login'));
+app.use(identityPath, require('../src/routes/identity-routes'));
 app.use(reminderPath, require('../src/routes/reminder-routes'));
 app.use(contactPath, require('../src/routes/contact-routes'));
 app.use(appointmentPath, require('../src/routes/appointment-routes'));
-app.use(profilePath, require('../src/routes/profile-routes'));
 export default app;
