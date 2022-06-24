5c2ac087462895f1dfb97b99d889cc130ce5729e
00146520 <00146520@uca.edu.sv>
2022-06-23 22:30:05 -0700

    feat: base design for login, register and recover

---
+++ notimed-admin-web/src/components/Buttons/LogInButton.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import { ButtonProps } from "../interfaces/props";
+
+const LogInButton: FC<ButtonProps> = ({ className, text }) => {
+    return (
+        <button className="bg-primaryContainer text-onPrimaryContainer labelLarge w-1/2 rounded-full" type="submit">
+            <div className="hover:bg-onPrimaryContainerState-hover focus:bg-onPrimaryContainerState-focus
+                px-7 py-2 w-full rounded-full">
+                {text}
+            </div>
+        </button>
+    )
+}
+
+export default LogInButton;
