84c31ccb755c133f7773c0fe9cfdc48b3fccc6e5
rodrigo1619 <00067920@uca.edu.sv>
2022-06-29 17:50:42 -0600

    chore: more cards added to ContactRepository

--- notimed-android-app/app/src/main/java/com/mrroboto/notimed/repositories/ContactRepository.kt
+++ notimed-android-app/app/src/main/java/com/mrroboto/notimed/repositories/ContactRepository.kt
@@ -4,7 +4,10 @@ import com.mrroboto.notimed.data.models.Contact
 
 class ContactRepository {
     private var _contacts = listOf(
-        Contact("Fernando", "1234-5678", "Opico", "General","12:00", "13:00")
+        Contact("Fernando", "1234-5678", "Opico", "General","12:00PM", "13:00PM"),
+        Contact("Juan", "1234-5679", "Opico", "Ginecologo","1:00PM", "17:00PM"),
+        Contact("Wilmer", "2234-5678", "Santa tecla", "General","16:00PM", "18:00PM"),
+        Contact("Rodrigo", "1134-5678", "Soyapango", "General","10:00AM", "11:00AM")
     ).toMutableList()
 
     val contacts: MutableLiveData<List<Contact>>
