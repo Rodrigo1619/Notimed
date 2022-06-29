6ca013375d6f128dfa2ab11c17ce98c264b065d3
rodrigo1619 <00067920@uca.edu.sv>
2022-06-29 14:55:40 -0600

    feat: contact-model and contact-repository added

--- notimed-android-app/app/src/main/java/com/mrroboto/notimed/repositories/ContactRepository.kt
+++ notimed-android-app/app/src/main/java/com/mrroboto/notimed/repositories/ContactRepository.kt
@@ -1,4 +1,17 @@
 package com.mrroboto.notimed.repositories
+import androidx.lifecycle.MutableLiveData
+import com.mrroboto.notimed.data.models.Contact
 
 class ContactRepository {
+    private var _contacts = listOf(
+        Contact("Fernando", "1234-5678", "Opico", "General","12:00", "13:00")
+    ).toMutableList()
+
+    val contacts: MutableLiveData<List<Contact>>
+        get() = MutableLiveData(_contacts)
+
+    fun addContact(contact: Contact){
+        _contacts.add(contact)
+        contacts.value = _contacts
+    }
 }
