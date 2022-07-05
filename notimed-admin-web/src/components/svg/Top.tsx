27f7f36f4dcb4c6044d7a9935363430950a7d902
fernanortega-hub <00191820@uca.edu.sv>
2022-07-05 14:06:51 -0600

    fix: recover design

--- notimed-admin-web/src/components/svg/Top.tsx
+++ notimed-admin-web/src/components/svg/Top.tsx
@@ -4,7 +4,7 @@ import UserNotimed from "./UserNotimed";
 
 const Top: FC<TopVector> = ({ className, title, icon }) => {
     return (
-        <div className="w-full py-14 flex flex-wrap items-center bg-primary mb-8 md:py-20 lg:mb-14">
+        <div className={`w-full py-14 flex flex-wrap items-center bg-primary mb-8 md:py-20 lg:mb-14 ${className}`}>
             <div className="flex flex-wrap items-center absolute w-full justify-center space-x-4 text-center">
                 <span className="w-auto text-onPrimary text-headline4 h-auto"> {title} </span>
                 {icon}
