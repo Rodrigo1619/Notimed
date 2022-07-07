ba503af6cfb3c9eeb3a008c89cfd1247fc30cc5e
rodrigo1619 <00067920@uca.edu.sv>
2022-07-07 17:45:24 -0600

    feat: Appointmentviewmodel updated

--- notimed-android-app/app/src/main/java/com/mrroboto/notimed/viewmodels/AppointmentViewModel.kt
+++ notimed-android-app/app/src/main/java/com/mrroboto/notimed/viewmodels/AppointmentViewModel.kt
@@ -1,11 +1,90 @@
 package com.mrroboto.notimed.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mrroboto.notimed.data.models.Appointment
+import com.mrroboto.notimed.network.ApiResponse
+import com.mrroboto.notimed.network.responses.appointment.OneAppointmentResponse
 
 import com.mrroboto.notimed.repositories.AppointmentRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class AppointmentViewModel(private val repository: AppointmentRepository): ViewModel() {
-    val appointments = repository.appointments
+    var currentName = MutableLiveData<String>()
+    var currentMedic = MutableLiveData<String>()
+    var currentDate = MutableLiveData<String>()
+    var currentHour = MutableLiveData<String>()
+    var currentLocalization = MutableLiveData<String>()
+    var CurrentConsiderations = MutableLiveData<String>()
+
+    val apiResponse = MutableLiveData<ApiResponse<Any>>()
+    val listResponse = MutableLiveData<ApiResponse<List<Appointment>>>()
+    val response = MutableLiveData<ApiResponse<OneAppointmentResponse>>()
+
+    fun createAppointment(
+        isLoading: Boolean,
+        name:String,
+        medic:String,
+        date: String,
+        hour: String,
+        localization: String,
+        considerations: String
+    ) = viewModelScope.launch {
+        apiResponse.value = ApiResponse.Loading(isLoading)
+
+        apiResponse.postValue(
+            repository.addAppointment(
+                name,
+                medic,
+                date,
+                hour,
+                localization,
+                considerations
+            )
+        )
+    }
+    fun getAppointments(isLoading: Boolean) = viewModelScope.launch {
+        apiResponse.value = ApiResponse.Loading(isLoading)
+
+        listResponse.postValue(repository.getAppointments())
+    }
+    fun deleteAppointments(id:String)=viewModelScope.launch {
+        apiResponse.value = ApiResponse.Loading(isLoading = true)
+
+        apiResponse.postValue(repository.deleteAppointment(id))
+    }
+    fun updateAppointment(
+        id:String,
+        name:String,
+        medic: String,
+        date: String,
+        hour: String,
+        localization: String,
+        considerations: String
+    ){
+        apiResponse.value = ApiResponse.Loading(isLoading = true)
+        viewModelScope.launch(Dispatchers.IO) {
+            apiResponse.postValue(
+                repository.updateAppointment(
+                    name,
+                    medic,
+                    date,
+                    hour,
+                    localization,
+                    considerations,
+                    id
+                )
+            )
+        }
+    }
+
+    fun getOneAppointment(id:String) = viewModelScope.launch {
+        apiResponse.value = ApiResponse.Loading(isLoading = true)
+
+        response.postValue(repository.getOneAppointment(id))
+    }
 
 }
