c3262603972305ce482c9ed0af2811f3e4de2bf2
rodrigo1619 <00067920@uca.edu.sv>
2022-07-07 15:42:52 -0600

    feat: Reminder model updated

--- notimed-android-app/app/src/main/java/com/mrroboto/notimed/data/models/Appointment.kt
+++ notimed-android-app/app/src/main/java/com/mrroboto/notimed/data/models/Appointment.kt
@@ -1,10 +1,17 @@
 package com.mrroboto.notimed.data.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "appointments_table")
 data class Appointment(
-    val Name: String,
-    val Medic: String,
-    val Date: String,
-    val Hour: String,
-    val Localization: String,
-    val Considerations: String
+    @PrimaryKey
+    @ColumnInfo("_id") val _id: String,
+    @ColumnInfo("name") val Name: String,
+    @ColumnInfo("medic") val Medic: String,
+    @ColumnInfo("date") val Date: String,
+    @ColumnInfo("hour") val Hour: String,
+    @ColumnInfo("localization") val Localization: String,
+    @ColumnInfo("considerations") val Considerations: String
 )
