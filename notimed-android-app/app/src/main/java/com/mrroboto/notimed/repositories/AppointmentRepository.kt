bbdf7e62a8c7b723293ff736dcd22c4227c5ddc8
rodrigo1619 <00067920@uca.edu.sv>
2022-07-07 17:16:05 -0600

    feat: appointment repository updated

--- notimed-android-app/app/src/main/java/com/mrroboto/notimed/repositories/AppointmentRepository.kt
+++ notimed-android-app/app/src/main/java/com/mrroboto/notimed/repositories/AppointmentRepository.kt
@@ -1,21 +1,90 @@
 package com.mrroboto.notimed.repositories
 
 import androidx.lifecycle.MutableLiveData
+import com.mrroboto.notimed.data.AppDatabase
 import com.mrroboto.notimed.data.models.Appointment
+import com.mrroboto.notimed.network.ApiResponse
+import com.mrroboto.notimed.network.responses.appointment.AppointmentRequest
+import com.mrroboto.notimed.network.responses.appointment.OneAppointmentResponse
+import com.mrroboto.notimed.network.services.AppointmentService
+import retrofit2.HttpException
 
-class AppointmentRepository {
-    private var _appointments = listOf(
-        Appointment("Gripe", "4:30pm" ,"10-07-2022", "1:00pm", "Antiguo Cuscatlan, Calle #5, Clinica MemoMed", "llevar sueter"),
-        Appointment("Indigestion", "5:30pm","15-07-2022", "2:00pm", "Antiguo Cuscatlan, Calle #5, Clinica MemoMed", "no comer cosas solidas un dia antes"),
-        Appointment("Dolor de cabeza", "8:00am","09-07-2022", "1:00pm", "Antiguo Cuscatlan, Calle #5, Clinica MemoMed","llevar el doloneurobion"),
-        Appointment("Dolor de espalda", "11:00am","09-07-2022", "2:00pm", "Antiguo Cuscatlan, Calle #5, Clinica MemoMed", "")
-    ).toMutableList()
+class AppointmentRepository (
+    private val api: AppointmentService,
+    database: AppDatabase,
+    private var user_id: String
+){
 
-    val appointments: MutableLiveData<List<Appointment>>
-        get() = MutableLiveData(_appointments)
+    private val appointmentDao = database.appointmentDao()
 
-    fun addReminder(reminder: Appointment){
-        _appointments.add(reminder)
-        appointments.value = _appointments
+    suspend fun addAppointment(
+        name:String,
+        medic:String,
+        date:String,
+        hour:String,
+        localization:String,
+        considerations: String
+    ):ApiResponse<Any>{
+        return try{
+            val response = api.createAppointment(
+                user_id,
+                AppointmentRequest(name, medic, date, hour, localization, considerations)
+            )
+            ApiResponse.Success(response)
+        }catch (err: HttpException){
+            ApiResponse.Failure(err.code(),err.message())
+        }
+    }
+
+    suspend fun getAppointments():ApiResponse<List<Appointment>>{
+        return try{
+            val response = api.getAppointment(user_id)
+
+            if(response.total>0){
+                for(appointments in response.appointments){
+                    appointmentDao.insertAppointment(appointments)
+                }
+            }
+            ApiResponse.Success(data = appointmentDao.getAllAppointments())
+        }catch (err:HttpException){
+            ApiResponse.Failure(err.code(), err.message())
+        }
+    }
+
+    suspend fun deleteAppointment(cardId: String): ApiResponse<Any>{
+        return try{
+            val response = api.deleteAppointment(cardId, user_id)
+            appointmentDao.deleteAppointment(cardId)
+            ApiResponse.Success(response)
+        }catch (err: HttpException){
+            ApiResponse.Failure(err.code(), err.message())
+        }
+    }
+
+    suspend fun updateAppointment(
+        name: String,
+        medic: String,
+        date: String,
+        hour: String,
+        localization: String,
+        considerations: String,
+        cardId:String
+    ): ApiResponse<Any>{
+        return try{
+            val response = api.updateAppointment(cardId, user_id, AppointmentRequest(name,medic,date,hour,localization,considerations))
+            appointmentDao.insertAppointment(Appointment(cardId,name, medic, date, hour, localization, considerations))
+            ApiResponse.Success(response)
+        }catch (err: HttpException){
+            ApiResponse.Failure(err.code(), err.message())
+        }
+    }
+    suspend fun getOneAppointment(id:String):ApiResponse<OneAppointmentResponse>{
+        return try{
+            val response = api.getOneAppointment(id, user_id)
+            ApiResponse.Success(response)
+        }catch (err: HttpException){
+            ApiResponse.Failure(err.code(), err.message())
+        }
     }
 }
+
