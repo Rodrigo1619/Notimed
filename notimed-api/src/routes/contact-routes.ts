1208eee0889c6a4af098a0665bac7f2b285a95dc
fernanortega-hub <00191820@uca.edu.sv>
2022-07-08 20:55:23 -0600

    fix: ids in contact

--- notimed-api/src/routes/contact-routes.ts
+++ notimed-api/src/routes/contact-routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 //get
 router.get('/:id', getContacts);
 router.get('/:id/:id2', [
-    check('id1', 'No es un id valido').isMongoId(),
+    check('id', 'No es un id valido').isMongoId(),
     check('id2', 'No es un id valido').isMongoId()
 ], getContact);
 //post
@@ -34,12 +34,12 @@ router.post('/create/:id', [
 
 //delete
 router.delete('/delete/:id/:id2', [
-    check('id1', 'No es un id valido').isMongoId(),
+    check('id', 'No es un id valido').isMongoId(),
     check('id2', 'No es un id valido').isMongoId()
 ], deleteContact);
 //patch
 router.patch('/update/:id/:id2', [
-    check('id1', 'No es un id valido').isMongoId(),
+    check('id', 'No es un id valido').isMongoId(),
     check('id2', 'No es un id valido').isMongoId()
 ], updateContact);
 
